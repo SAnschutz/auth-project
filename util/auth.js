ad2cde133002df58cf9ebdaf9a9173fb929227e2
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2022-07-25 00:54:58 -0400

    initial commit

---
+++ util/auth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { API_KEY } from '@env';
+
+export async function createUser(email, password) {
+  console.log(API_KEY);
+  //making it async means a promise will be returned
+  const response = await axios.post(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+  );
+}
