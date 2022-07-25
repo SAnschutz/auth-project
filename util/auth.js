fef99e72b78c2ff95cac632de62173bb014151ae
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2022-07-25 18:07:03 -0400

    login logic

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import { API_KEY } from '@env';
 
+async function authenticate(mode, email, password){
+  const url=`https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
+
+  await axios.post(url, {email, password, returnSecureToken: true})
+}
+
 export async function createUser(email, password) {
   //making it async means a promise will be returned
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email,
-      password,
-      returnSecureToken: true,
-    }
-  );
+ await authenticate('signUp', email, password)
+
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password)
 }
