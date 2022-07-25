f594bb1ac0209e524f3c315cabab210f3b3bfbed
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2022-07-25 01:36:02 -0400

    fix auth.js

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { API_KEY } from '@env';
 
 export async function createUser(email, password) {
-  console.log(API_KEY);
   //making it async means a promise will be returned
   const response = await axios.post(
     `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
